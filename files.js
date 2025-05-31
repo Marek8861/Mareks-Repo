51685b0bf0ab212d49afdbcf10385b1c20f2519d
Marek8861 <144486893+Marek8861@users.noreply.github.com>
2025-05-31 23:19:35 +0200

    d

---
+++ files.js
@@ -0,0 +1,13 @@
+// Struktura plików i folderów - nazwa: ścieżka lub kolejny obiekt (folder)
+const files = {
+  "index.html": "https://raw.githubusercontent.com/Marek8861/Mareks-Repo/main/index.html",
+  "style.css": "https://raw.githubusercontent.com/Marek8861/Mareks-Repo/main/style.css",
+  "files.js": "https://raw.githubusercontent.com/Marek8861/Mareks-Repo/main/files.js",
+  "repo": {
+    "README.md": "https://raw.githubusercontent.com/Marek8861/Mareks-Repo/main/repo/README.md",
+    "script.js": "https://raw.githubusercontent.com/Marek8861/Mareks-Repo/main/repo/script.js",
+    "subfolder": {
+      "data.json": "https://raw.githubusercontent.com/Marek8861/Mareks-Repo/main/repo/subfolder/data.json"
+    }
+  }
+};
